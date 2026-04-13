c9098eeec81e1e21e90e86ae3a8cc28dd4ff95c7
ucup233 <sufhidayat9@gmail.com>
2026-04-13 13:38:29 +0700

    Initial commit

---
+++ src/main/java/com/example/praktikum7/Praktikum7Application.java
@@ -0,0 +1,13 @@
+package com.example.praktikum7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Praktikum7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Praktikum7Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/praktikum7/Praktikum7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.praktikum7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Praktikum7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
