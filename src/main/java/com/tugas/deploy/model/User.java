3deaa513436b73e6ebeb595cb8a24da4b68d10bf
ucup233 <sufhidayat9@gmail.com>
2026-04-13 14:16:36 +0700

    Pembuatan model untuk mahasiswa

---
+++ src/main/java/com/tugas/deploy/model/User.java
@@ -0,0 +1,16 @@
+package com.tugas.deploy.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    private String nim;
+    private String nama;
+    private String jenisKelamin;
+
+}
